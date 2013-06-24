3fe098bbd0d9efe24175faf45b6184a85eeb896f
Geoffrey Smalling <cedralpass@gmail.com>
2013-06-24 08:07:33 -0700

    updated trip to belong to user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should have a password" do
+    user = FactoryGirl.create(:user)
+    user.should be_an_instance_of(User)
+    user.password.should_not be_nil
+  end
+  it "should have an email" do
+      user = FactoryGirl.create(:user)
+      user.should be_an_instance_of(User)
+      user.email.should_not be_nil
+    end
 end
