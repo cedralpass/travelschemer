1164257f795a6e5c66e016f3c66897cc77c80c39
Geoffrey Smalling <cedralpass@gmail.com>
2013-06-24 08:19:58 -0700

    added home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Travelschemer::Application.routes.draw do
 
-
+  get "page/home"
 
   devise_for :users
 
@@ -60,8 +60,8 @@ Travelschemer::Application.routes.draw do
   #   end
 
   # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  # just remember to delete public/rails_index.html.
+   root :to => 'page#home'
 
   # See how all your routes lay out with "rake routes"
 
